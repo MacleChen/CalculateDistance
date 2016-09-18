712070ad4b2d66aaf382a01cd36521f07e747cc6
victor8200 <liulxzj@163.com>
2016-09-18 16:14:52 +0800

    Initial Commit

---
+++ CalculateDistance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculateDistance
+//
+//  Created by jointsky on 16/9/18.
+//  Copyright © 2016年 陈帆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
